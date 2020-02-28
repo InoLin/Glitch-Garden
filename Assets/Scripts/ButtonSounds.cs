3c6de01dc0d4ef31f999c7391f5f390818c3f0b8
InoLin <baseball021007@gmail.com>
2020-02-28 18:17:00 +0800

    [Ino] 1.新增按鈕音效功能 2.戰鬥暫停選單製作中

---
+++ Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour
+{
+    public AudioSource buttonAudio;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
+
+    public void HoverSound()
+    {
+        buttonAudio.PlayOneShot(hoverSound);
+    }
+
+    public void ClickSound()
+    {
+        buttonAudio.PlayOneShot(clickSound);
+    }
+
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pausedMenuUI;
+    public SoundLoader soundLoader;
+
+    void Start()
+    {
+        soundLoader = FindObjectOfType<SoundLoader>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        soundLoader.gameObject.GetComponent<AudioSource>().Play();
+        pausedMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        soundLoader.gameObject.GetComponent<AudioSource>().Pause();
+        pausedMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
