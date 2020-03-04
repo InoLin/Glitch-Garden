4fb3bfba10ffe47307a2fe1a0a9d28aaae1a4999
InoLin <baseball021007@gmail.com>
2020-03-04 21:12:58 +0800

    [Ino] 1.新增fungus套件 2.人物對話第一章完成 3.Loading bar可得到載入下個場景的值做出真實跑條

---
+++ Assets/Scripts/AsyncManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AsyncManager : MonoBehaviour
+{
+    AsyncOperation async;
+    public Slider loadingSlider;
+
+    void Start()
+    {
+        async = SceneManager.LoadSceneAsync("Level 01");
+        async.allowSceneActivation = false;
+    }
+
+    void Update()
+    {
+        loadingSlider.value = async.progress;
+
+        if (async.progress >= 0.9f)
+        {
+            async.allowSceneActivation = true;
+        }
+    }
+}
